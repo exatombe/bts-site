235022065c90a3fc77492eccfa84297190f119e1
garder500 <jeremy27.clara22@gmail.com>
2022-02-17 17:36:15 +0100

    Update

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html lang="fr">
     <head>
-        <link rel="stylesheet" type="text/css" href="style.css"/>
-            <title>
-                Test
-            </title>
+       <?
+       $n = "MangaShop"; // Titre
+       $content = "Mon super site "; // Description
+       $image ="https://img.search.brave.com/H1k51jh7MoZP6ORpCXv7mD-vgVSQciaZIwBtoBkNrqg/rs:fit:400:400:1/g:ce/aHR0cHM6Ly9wYnMu/dHdpbWcuY29tL3By/b2ZpbGVfaW1hZ2Vz/LzE1NjM2OTU4ODIv/bG9nb19ibG9nXzQw/MHg0MDAucG5n"; // Image
+       $key = "manga, scan, shop, e-commerce, one piece, naruto"; // Mots clé
+       include "./parts/head.php";
+       ?>
     </head>
     <body>
         <h1>Salut</h1>
