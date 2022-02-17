28979d81e01e7831fb2e583d2ccbdcc4f87dfb64
garder500 <jeremy27.clara22@gmail.com>
2022-02-17 15:25:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+        Test
+        </title>
+    </head>
+    <body>
+    <h1>Salut</h1>
+    <?= "Salut"; ?>
+    <p>Putain je suis perdu dans ton truc, ho bordel ça lag un peu en plus quoi</p>
+    </body>
+</html>
