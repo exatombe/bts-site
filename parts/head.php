b3999bb66036684763a81f3589d2bcadcd528df9
garder500 <jeremy27.clara22@gmail.com>
2022-02-17 17:44:31 +0100

    Update

--- parts/head.php
+++ parts/head.php
@@ -1,39 +1,41 @@
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-<meta name="description" content="<?php echo $content; ?>"/>
-<meta name="mobile-web-app-capable" content="yes" />
-<meta name="application-name" content="AnimeHub" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-title" content="AnimeHub" />
-<meta property="og:title" content="<?php echo $n; ?>" />
-<meta property="og:type" content="website" />
-<meta property="og:site_name" content="AnimeHub" />
-<meta name="keywords" content="<?php echo $key; ?>" />
-<meta property="og:url" content="https://animehub.world-heberg.com/" />
-<meta property="og:image" content="<?php echo $image; ?>" />
-<meta property="og:description" content="<?php echo $content; ?>"/>
-<meta name="twitter:card" content="summary" />
-<meta name="twitter:site" content="@diamondeagle8" />
-<meta name="twitter:title" content="<?php echo $n; ?>" />
-<meta name="twitter:description" content= "<?php echo $content; ?>" />
-<title><?php echo $n; ?></title>
+<?php
+    if(isset($content) && isset($n) && isset($key) && isset($image)){
+?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="description" content="<?php echo $content; ?>"/>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="AnimeHub" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="AnimeHub" />
+        <meta property="og:title" content="<?php echo $n; ?>" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="AnimeHub" />
+        <meta name="keywords" content="<?php echo $key; ?>" />
+        <meta property="og:url" content="https://animehub.world-heberg.com/" />
+        <meta property="og:image" content="<?php echo $image; ?>" />
+        <meta property="og:description" content="<?php echo $content; ?>"/>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@diamondeagle8" />
+        <meta name="twitter:title" content="<?php echo $n; ?>" />
+        <meta name="twitter:description" content= "<?php echo $content; ?>" />
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <!-- Le meta magique pour les appareils mobiles :)-->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=4">
+        <meta name="robots" content="index, follow">
+        <meta name="theme-color" content="#1a1a1a"/>
+        <link rel="icon" type="image/png" sizes="64x64" href="https://bot-creator.com/favicon-64.png">
+        <title><?php echo $n; ?></title>
 <!-- Font Awesome -->
-<link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-        rel="stylesheet"
-/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"/>
 <!-- Google Fonts -->
-<link
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        rel="stylesheet"
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"
 />
 <!-- MDB -->
-<link
-        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css"
-        rel="stylesheet"
-/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css" rel="stylesheet"/>
 <!-- MDB -->
-<script
-        type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
-></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"></script>
+<?php
+    }
+    ?>
