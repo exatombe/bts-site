7ba693ce03d66de62d17ecb3faf36214b1c7d2ee
garder500 <jeremy27.clara22@gmail.com>
2022-02-17 16:26:32 +0100

    sdsqs

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
 <html lang="fr">
-<link rel="stylesheet" type="text/css" href="style.css"/>
-<title>
-    Test
-</title>
-<body>
-    <h1>Salut</h1>
-    <div >
-        Salut i'm an element
-    </div>
-    <div >
-        Salut i'm jerem and i'm cheum
-    </div>
-    <div >
-        Salut i'm an element proving that jerem is gay
-    </div>
-    <div >
-        Salut i'm an element
-    </div>
-    <div >
-        Salut i'm an element
-    </div>
-    <div >
-        Salut i'm an element
-    </div>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+            <title>
+                Test
+            </title>
+    </head>
+    <body>
+        <h1>Salut</h1>
     </body>
 </html>
