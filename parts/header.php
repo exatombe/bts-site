436b51e63bb273245b29e1f205268087fe1038a8
garder500 <jeremy27.clara22@gmail.com>
2022-03-17 14:27:51 +0100

    gg

--- parts/header.php
+++ parts/header.php
@@ -1,13 +1,13 @@
 <header class="head">
-<p>Test</p>
+	<p>Test</p>
 
-<ul>
-	<li><a>aaa</a></li>
-	<li><a>bbb</a></li>
-	<li><a>ccc</a></li>
-	<li><a>ddd</a></li>
-</ul>
+	<ul>
+		<li><a>aaa</a></li>
+		<li><a>bbb</a></li>
+		<li><a>ccc</a></li>
+		<li><a>ddd</a></li>
+	</ul>
 
-<i class="fa-solid fa-bag-shopping"></i>
-<a href="./parts/Authentification.php" style="color: white;"><i class="fa-solid fa-circle-user" style="font-size: 30px; margin-left: 1100px; margin-top: -95px; border-left: solid 1px white; padding-left: 20px; cursor: pointer;"></i></a>
-</header>
+	<i class="fa-solid fa-bag-shopping"></i>
+	<a href="./parts/Authentification.php" style="color: white;"><i class="fa-solid fa-circle-user" style="font-size: 30px; margin-left: 1100px; margin-top: -95px; border-left: solid 1px white; padding-left: 20px; cursor: pointer;"></i></a>
+</hea§/der>
