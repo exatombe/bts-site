46db5edfdbccb8ed5c9ee80867bd95137b40a09b
Sukeshi7 <matheo.saiter@hotmail.fr>
2022-03-18 17:50:07 +0100

    bobb

--- parts/header.php
+++ parts/header.php
@@ -1,13 +1,14 @@
 <header class="head">
 	<p>Test</p>
 
-    <div class="menu"
+    <div class="menu">
 	    <ul>
 		    <li><a>aaa</a></li>
 		    <li><a>bbb</a></li>
 		    <li><a>ccc</a></li>
 		    <li><a>ddd</a></li>
 	    </ul>
+    </div>
 
 	<a href="http://<?= $_SERVER['SERVER_NAME']; ?>/dashboard/authentification" style="color: white; font-size: 35px;"><h6><i class="fa-solid fa-circle-user"></i>Connexion</h6></a>
 </header>
