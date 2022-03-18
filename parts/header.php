fdb6cd872490d737b901cefbee4f0e8eaf24c0d7
Vadim <arhgi129@gmail.com>
2022-03-18 17:46:22 +0100

    Changes by matheo

--- parts/header.php
+++ parts/header.php
@@ -1,12 +1,17 @@
 <header class="head">
 	<p>Test</p>
 
-	<ul>
-		<li><a>aaa</a></li>
-		<li><a>bbb</a></li>
-		<li><a>ccc</a></li>
-		<li><a>ddd</a></li>
-	</ul>
+    <div class="menu"
+	    <ul>
+		    <li><a>aaa</a></li>
+		    <li><a>bbb</a></li>
+		    <li><a>ccc</a></li>
+		    <li><a>ddd</a></li>
+	    </ul>
 
 	<a href="http://<?= $_SERVER['SERVER_NAME']; ?>/dashboard/authentification" style="color: white; font-size: 35px;"><h6><i class="fa-solid fa-circle-user"></i>Connexion</h6></a>
 </header>
+
+<!--
+
+-->
