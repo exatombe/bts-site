4089e777366f6fd36687aaf1f9c3a0f33caa9a84
Sukeshi7 <matheo.saiter@hotmail.fr>
2022-03-24 15:47:27 +0100

    bobb

--- parts/header.php
+++ parts/header.php
@@ -1,15 +1,15 @@
 <header class="head">
-	<p>Le marché du manga</p>
+    <p>Le marché du manga</p>
 
     <div class="menu">
-	    <ul>
-		    <li><a>aaa</a></li>
-		    <li><a>bbb</a></li>
-		    <li><a>ccc</a></li>
-		    <li><a>ddd</a></li>
-	    </ul>
+        <ul>
+            <li><a>aaa</a></li>
+            <li><a>bbb</a></li>
+            <li><a>ccc</a></li>
+            <li><a>ddd</a></li>
+        </ul>
     </div>
-	<a href="http://<?= $_SERVER['SERVER_NAME']; ?>/dashboard/authentification" style="color: white; font-size: 35px;"><h6><i class="fa-solid fa-circle-user"></i>Connexion</h6></a>
+    <a href="http://<?= $_SERVER['SERVER_NAME']; ?>/dashboard/authentification" style="color: white; font-size: 35px;"><h6><i class="fa-solid fa-circle-user"></i>Connexion</h6></a>
 </header>
 
 <!--
