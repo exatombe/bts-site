3912e5ee3851185fdf2dac1a5430b883a7328449
Sukeshi7 <matheo.saiter@hotmail.fr>
2022-04-07 14:48:55 +0200

    bobb

---
+++ article.php
@@ -0,0 +1,37 @@
+<html lang="fr">
+    <head>
+        <?php
+        $n = "Le Marche des Mangas"; // Titre
+        $content = "Mon super site "; // Description
+        $image ="https://img.search.brave.com/H1k51jh7MoZP6ORpCXv7mD-vgVSQciaZIwBtoBkNrqg/rs:fit:400:400:1/g:ce/aHR0cHM6Ly9wYnMu/dHdpbWcuY29tL3By/b2ZpbGVfaW1hZ2Vz/LzE1NjM2OTU4ODIv/bG9nb19ibG9nXzQw/MHg0MDAucG5n"; // Image
+        $key = "manga, scan, shop, e-commerce, one piece, naruto"; // Mots clé
+        require "./parts/head.php";
+
+        ?>
+    </head>
+    <body style="background-color: lightgray;">
+        <?php include "./parts/header.php"; ?>
+
+        <div class="article">
+            <p class="article_titre">Fairy Tail - Tome 1</p>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            <br/><br/><br/>
+            rouge
+        </div>
+
+        <?php include './parts/footer.php'; ?>
+    </body>
+</html>
