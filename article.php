b32074c0399a3c2bb6f9d4e023f7de0378e34a8e
Sukeshi7 <matheo.saiter@hotmail.fr>
2022-04-11 15:19:18 +0200

    bobb

--- article.php
+++ article.php
@@ -13,9 +13,10 @@
         <?php include "./parts/header.php"; ?>
 
         <div class="article">
-
-            <p class="article_titre">Fairy Tail - Tome 1</p>
             <img class="article_img" src="/media/cover/Fairy%20Tail%20-%20Tome%201.jpg">
+            <div class="article_infos1">
+                <p class="article_titre">Fairy Tail - Tome 1</p>
+            </div>
             <br/><br/><br/>
             <br/><br/><br/>
             <br/><br/><br/>
