d21300f154cb60b11c12b0744a4bae675e053884
Vadim <arhgi129@gmail.com>
2022-04-21 15:29:28 +0200

    Update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <?php
     $n = "Le Marche des Mangas"; // Titre
     $content = "Mon super site "; // Description
-    $image ="/media/favicon.ico"; // Image
+    $image = "https://img.search.brave.com/H1k51jh7MoZP6ORpCXv7mD-vgVSQciaZIwBtoBkNrqg/rs:fit:400:400:1/g:ce/aHR0cHM6Ly9wYnMu/dHdpbWcuY29tL3By/b2ZpbGVfaW1hZ2Vz/LzE1NjM2OTU4ODIv/bG9nb19ibG9nXzQw/MHg0MDAucG5n"; // Image
     $key = "manga, scan, shop, e-commerce, one piece, naruto"; // Mots clé
     require "./parts/head.php";
 
@@ -23,12 +23,9 @@
                 <article class="card">
                     <img class="card_img" src="<?= $manga->getImage(); ?>" alt="<?= $manga->getTitre(); ?>">
                     <div class="card_text">
-                        <p style="font-weight: bold;"><?=  $manga->getTitre(); ?></p><br />
+                        <p style="font-weight: bold;"><?= $manga->getTitre(); ?></p><br />
                         <p> de <?= $manga->getAuteur()->getNom() . " " . $manga->getAuteur()->getPrenom(); ?></p><br />
                         <p style="font-size: larger;"><?= $manga->getPrix(); ?>€</p><br />
-
-                    </div>
-                    <div class="card_buttons">                        
                         <div class="button">
                             <p>AJOUTER AU PANIER</p>
                         </div>
