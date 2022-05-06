6540acb1b43c9a28f1b65de719fbaf226177a4c7
garder500 <jeremy27.clara22@gmail.com>
2022-05-06 16:27:53 +0200

    Push datas Password gen

--- dashboard/authentification.php
+++ dashboard/authentification.php
@@ -29,18 +29,18 @@
 <body>
 <div id="pageMessages">
 
-</div>
+</div>            <button style="padding:none; font-size:10px;" onclick="fillPassword()">Fill password</button>
+
     <div class="main">
         <input type="checkbox" id="chk" aria-hidden="true">
-
         <div class="signup">
             <form method="POST">
                 <label for="chk" aria-hidden="true">Sign up</label>
                 <input type="hidden" name="signup" value="submit">
                 <input type="text" name="username" placeholder="Username" required="">
                 <input type="email" name="email" placeholder="Email" required="">
-                <input type="password" name="password" placeholder="Password" required="">
-                <input type="password" name="confirmPass" placeholder="Password" required="">
+                <span><input type="password" name="password" id="passwd" placeholder="Password" required=""></span>
+                <input type="password" name="confirmPass" id="passwd2" placeholder="Password" required="">
                 <input type="file" name="photo de ta sale gueule" placeholder="photo" required="">
                 <?php
                 if (isset($_POST['signup'])) {
@@ -101,6 +101,30 @@
             </form>
         </div>
     </div>
+    <script type="text/javascript">
+        let pwd = document.getElementById("passwd"),
+            pwd2 = document.getElementById("passwd2");
+
+      // make a function to gen a password with a length of 8 characters,
+        // with at least one number, one uppercase and one lowercase letter and one special character
+        // and not three of the same character in a row
+        function generatePassword() {
+            let length = 8,
+                charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+",
+                retVal = "";
+            for (let i = 0, n = charset.length; i < length; ++i) {
+                retVal += charset.charAt(Math.floor(Math.random() * n));
+            }
+            return retVal;
+        }
+       
+        // make a function to fill the password field with a generated password
+        function fillPassword() {
+            let passwordCurrent = generatePassword();
+            pwd.value = passwordCurrent;
+            pwd2.value = passwordCurrent;
+        }
+    </script>
 </body>
 
 </html>
