4979687c58403963d396e6f9d222ba374a05ef1d
garder500 <jeremy27.clara22@gmail.com>
2022-06-01 22:54:51 +0200

    Publish (and commenting who have doing something)

--- presentation.php
+++ presentation.php
@@ -19,43 +19,70 @@
     Du shônen en passant par le seinen, du shojo au hentai nous possédons un grand nombre de genre
     et de licence pour tous et pour toutes.</h5>
 <br />
-<br />
-<br />
-<div class="imgbox">
-    <img class="jerem" src="media/jeremy.jpg" alt="un mec moche à lunette" />
-    <img class="maths" src="media/matteo.jpg" alt="un mec moche mais qui est fort en maths" />
-    <img class="vad" src="media/vadim.jpg" alt="un mec moche asiatique" />
-    <img class="moi" src="media/tristan.jpg" alt="un mec moche et c'est tout" />
+
+
+<div class="container-box">
+  <div class="card-box">
+    <img  src="/media/jeremy.jpg" alt="un mec moche à lunette" class="card_box_image">
+    <p class="card_box_name">Jeremy Soler</p>
+    <div class="sub_card_name">
+        Chef de projet, amateur de loli depuis 2003, "FaitTout" man
+    </div>
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+      <li><a href="#"><i class="fa fa-codepen"></i></a></li>
+    </ul>
+    <p>« J'ai une super idée faudra juste changer tou... Mathéo pose ce couteau »</p>
+  </div>
+  <div class="card-box">
+    <img  src="/media/matteo.jpg" alt="un mec moche mais qui est fort en maths" class="card_box_image">
+    <p class="card_box_name">Mathéo Saiter</p>
+    <div class="sub_card_name">
+        Pro en maths, a mis des centaines de chinois au chômage
+    </div>
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+      <li><a href="#"><i class="fa fa-codepen"></i></a></li>
+    </ul>
+    <p>« Jeremy tu fais chier »</p>
+  </div>
+  <div class="card-box">
+    <img  src="media/vadim.jpg" alt="un mec moche asiatique" class="card_box_image">
+    <p class="card_box_name">Vadim Glavieux</p>
+    <div class="sub_card_name">
+        Homme de culture
+    </div>
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+      <li><a href="#"><i class="fa fa-codepen"></i></a></li>
+    </ul>
+    <p>« j'ai fais le back mais j'ai rien compris »</p>
+  </div>
+  <div class="card-box">
+    <img src="media/tristan.jpg" alt="un mec moche et c'est tout"  class="card_box_image">
+    <p class="card_box_name">Tristan Tran</p>
+    <div class="sub_card_name">
+        Asiatique
+    </div>
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+      <li><a href="#"><i class="fa fa-codepen"></i></a></li>
+    </ul>
+    <p>« Brûlez moi celui qui a niqué le css. Ha c'est moi  »</p>
+  </div>
 </div>
 
-<div class="textimgbox1">
-    <span class="textimgboxcss">
-    <p style="font-size: larger;">Jeremy Soler</p> <br />
-    <p>Chef de projet, amateur de loli depuis 2003, "FaitTout" man</p> <br />
-    <p>« J'ai une super idée faudra juste changer tou... Mathéo pose ce couteau »</p>
-    </span>
-</div>
-<div class="textimgbox2">
-    <span class="textimgboxcss">
-    <p style="font-size: larger;">Mathéo Saiter</p> <br />
-    <p>Pro en maths, a mis des centaines de chinois au chômage</p> <br />
-    <p>« Jeremy tu fais chier »</p>
-    </span>
-</div>
-<div class="textimgbox3">
-    <span class="textimgboxcss">
-    <p style="font-size: larger;">Vadim Glavieux</p> <br />
-    <p>Homme de culture</p> <br />
-    <p>« j'ai fais le back mais j'ai rien compris »</p>
-    </span>
-</div>
-<div class="textimgbox4">
-    <span class="textimgboxcss">
-    <p style="font-size: larger;">Tristan Tran</p> <br />
-    <p>Asiatique</p> <br />
-    <p>« Brûlez moi celui qui a niqué le css. Ha c'est moi  »</p>
-    </span>
-</div>
+
+
+
 <?php include './parts/footer.php'; ?>
 </body>
 </html>
