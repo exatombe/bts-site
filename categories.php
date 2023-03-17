4d63c7c8770b03275c714f5ea817df0d7626830f
garder500 <jeremy27.clara22@gmail.com>
2023-03-17 13:35:04 +0100

    Fix des git merge de merde

--- categories.php
+++ categories.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html lang="fr">
 <head>
     <?php
@@ -64,71 +63,4 @@ if (count($mangas) > 0) {
 
 <?php include './parts/footer.php'; ?>
 </body>
-=======
-<html lang="fr">
-<head>
-    <?php
-    $title = "";
-    if(isset($_GET["id"])){
-        if($_GET["id"] == "shojo"){
-            $title = "Shojo";
-        }
-        else if($_GET["id"] == "seinen"){
-            $title = "Seinen";
-        }
-        else if($_GET["id"] == "shonen"){
-            $title = "Shonen";
-        }
-    }else{
-        header("Location: /");
-    }
-    $n = $title." - Le Marche des Mangas"; // Titre
-    $content = "Mon super site "; // Description
-    $image ="/media/favicon.ico"; // Image
-    $key = "manga, scan, shop, e-commerce, one piece, naruto"; // Mots clé
-    require "./parts/head.php";
-
-    ?>
-</head>
-<body style="background-color: lightgray;">
-<?php include "./parts/header.php"; ?>
-
-<div class="cards">
-<?php
-
-$mangas = $auth->findBy("manga",["Genre" => $title]);
-
-
-function makeMangaFromArray($value)
-{
-    return (new EntityRepository)->select(new Manga($value["ID_MANGA"]));
-}
-$mangas = array_map("makeMangaFromArray",$mangas);
-
-if (count($mangas) > 0) {
-    foreach ($mangas as $manga) {
-        ?>
-        <article class="card">
-            <a href="/article/<?php echo $manga->getId(); ?>">
-                <img class="card_img" src="<?= $manga->getImage(); ?>" alt="<?= $manga->getTitre(); ?>">
-            </a>
-            <div class="card_text">
-                <p style="font-weight: bold;"><?= strlen($manga->getTitre()) > 20 ? substr($manga->getTitre(),0,20)."..." : $manga->getTitre(); ?></p><br />
-                <p> de <?= $manga->getAuteur()->getNom() . " " . $manga->getAuteur()->getPrenom(); ?></p><br />
-                <p style="font-size: larger;"><?= $manga->getPrix(); ?>€</p><br />
-                <a class="button" href="/addToCart?id=<?= $manga->getId(); ?>">
-                    AJOUTER AU PANIER
-                </a>
-            </div>
-        </article>
-
-        <?php
-    }
-}
-?>
-</div>
-
-<?php include './parts/footer.php'; ?>
-</body>
->>>>>>> 2c76f1ceab2eac0250086e1d6dc774e39b7f1312
 </html>
