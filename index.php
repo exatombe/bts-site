60ad0602c9851075837f9660cc76849bc08696dc
Sukeshi7 <matheo.saiter@hotmail.fr>
2023-03-31 14:38:15 +0200

    correction affichage des manga pages globales et catégories

--- index.php
+++ index.php
@@ -24,19 +24,24 @@
                         <img class="card_img" src="<?= $manga->getImage(); ?>" alt="<?= $manga->getTitre(); ?>">
                     </a>
                     <div class="card_text">
-                        <p style="font-weight: bold;"><?= strlen($manga->getTitre()) > 20 ? substr($manga->getTitre(),0,20)."..." : $manga->getTitre(); ?></p><br />
-                        <p> de <?= $manga->getAuteur()->getNom() . " " . $manga->getAuteur()->getPrenom(); ?></p><br />
-                        <p style="font-size: larger;"><?= $manga->getPrix(); ?>€</p><br />
-                        <a class="button" href="/addToCart?id=<?= $manga->getId(); ?>">
+                        <p style="font-weight: bold;"><?= strlen($manga->getTitre()) > 20 ? substr($manga->getTitre(),0,20)."..." : $manga->getTitre(); ?></p>
+                        <p><?= $manga->getAuteur()->getNom() . " " . $manga->getAuteur()->getPrenom(); ?></p><br/>
+                        <p style="font-size: larger;"><?= $manga->getPrix(); ?>€</p><br/>
+                        <a class="add-to-cart-button" href="/addToCart?id=<?= $manga->getId(); ?>">
                             AJOUTER AU PANIER
                         </a>
                     </div>
                 </article>
 
+
         <?php
             }
         }
         ?>
+    </div>
+    <div style="text-align: center">
+        <a class="" style="margin: 10px" href="/outuveuxavantmonreuf">← Page precédente</a>
+        <a class="" style="margin: 10px" href="/outuveuxapresmonreuf">Page suivante →</a>
     </div>
     <?php include './parts/footer.php'; ?>
 </body>
